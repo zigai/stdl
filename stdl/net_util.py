5f5f7402593334c0def6bebd8f8b23a3b2fd30a4
ziga-ivansek <ziga.ivansek@protonmail.com>
2022-04-29 21:07:38 +0200

    change function name

--- stdl/net_util.py
+++ stdl/net_util.py
@@ -1,10 +1,9 @@
-import re
 from urllib.request import urlretrieve
 
 from tqdm import tqdm
 
 
-class TQDM_ProgressBar(tqdm):
+class ProgressBarTQDM(tqdm):
 
     def update_to(self, b=1, bsize=1, tsize=None):
         if tsize is not None:
@@ -12,11 +11,11 @@ class TQDM_ProgressBar(tqdm):
         self.update(b * bsize - self.n)
 
 
-def download(url: str, path: str, progressbar: bool = False):
+def download_file(url: str, path: str, progressbar: bool = False):
     succes = False
     try:
         if progressbar:
-            with TQDM_ProgressBar(unit="B", unit_scale=True, unit_divisor=1024) as t:
+            with ProgressBarTQDM(unit="B", unit_scale=True, unit_divisor=1024) as t:
                 urlretrieve(url, path, reporthook=t.update_to, data=None)
             t.total = t.n
 
