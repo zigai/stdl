5bb5b191a723fcca6617a5fc7fd46434c11eb8f2
zigai <ziga.ivansek@gmail.com>
2022-07-23 16:22:14 +0200

    bump project version, add minimum python version, formatting

--- setup.py
+++ setup.py
@@ -6,25 +6,26 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 REQUIREMENTS = (HERE / "requirements.txt").read_text()
 
-setup(name="stdl",
-      version="0.2.5",
-      description="Extended Python Standard Library",
-      long_description=README,
-      long_description_content_type="text/markdown",
-      author="Ziga Ivansek",
-      author_email="ziga.ivansek@gmail.com",
-      url="https://github.com/zigai/stdl",
-      license="MIT",
-      classifiers=[
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: MIT License",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 3.8",
-          "Programming Language :: Python :: 3.9",
-          "Programming Language :: Python :: 3.10",
-          "Programming Language :: Python :: 3 :: Only",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      packages=find_packages(),
-      install_requires=REQUIREMENTS)
+setup(
+    name="stdl",
+    version="0.3.5",
+    description="Extended Python Standard Library",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Ziga Ivansek",
+    author_email="ziga.ivansek@gmail.com",
+    url="https://github.com/zigai/stdl",
+    python_requires=">=3.10",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+)
