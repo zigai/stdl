15f26a07d4e7c297dad12043298413209e3591a0
zigai <ziga.ivansek@gmail.com>
2022-08-27 19:36:51 +0200

    feat: extended dataclass

---
+++ stdl/dataclass.py
@@ -0,0 +1,26 @@
+from dataclasses import asdict, astuple, dataclass
+from pprint import pprint
+
+
+class Data():
+
+    def __init__(self) -> None:
+        pass
+
+    @property
+    def dict(self) -> dict:
+        return asdict(self)
+
+    @property
+    def tuple(self) -> tuple:
+        return astuple(self)
+
+    def print(self):
+        pprint(self.dict)
+
+    def __getitem__(self, key):
+        return self.dict[key]
+
+    def __iter__(self):
+        for i in self.tuple:
+            yield i
