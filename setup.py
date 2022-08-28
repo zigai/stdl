ebec57711de59f18c5539c957671bdafcc13881b
zigai <ziga.ivansek@gmail.com>
2022-08-28 14:36:55 +0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ REQUIREMENTS = (HERE / "requirements.txt").read_text()
 
 setup(
     name="stdl",
-    version="0.3.5",
+    version="0.4.5",
     description="Extended Python Standard Library",
     long_description=README,
     long_description_content_type="text/markdown",
