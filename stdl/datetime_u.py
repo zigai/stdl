6ed855c3a688940c4441b3ad39b5aa4af5a3bba1
zigai <ziga.ivansek@gmail.com>
2022-11-22 23:49:32 +0100

    datetime_u docstrings

--- stdl/datetime_u.py
+++ stdl/datetime_u.py
@@ -1,8 +1,10 @@
 import time
-from datetime import date, datetime, timedelta
+from datetime import date, datetime, timedelta, timezone
 from typing import Generator
 
-from dateutil import parser
+from dateutil.parser import parse as parse_datetime_str
+
+local_tz = datetime.now().astimezone().tzinfo
 
 
 class Timer:
@@ -20,22 +22,43 @@ class Timer:
         return delta
 
 
-def parse_datetime(date: str) -> datetime:
-    return parser.parse(date)
-
-
 def fmt_datetime(
-    d: str | float | None | datetime = None,
+    d: str | float | int | None | datetime = None,
     fmt: str = "Ymd",
     d_sep: str = "-",
     t_sep: str = ":",
+    utc: bool = True,
 ) -> str:
+    """Format date and time
+
+    Args:
+        d (str | float | None | datetime, optional): Input datetime. Defaults to None.
+        fmt (str, optional): Date format. Defaults to "Ymd".
+        d_sep (str, optional): Date values seperator. Defaults to "-".
+        t_sep (_type_, optional): Time values seperator. Defaults to ":".
+        utc (bool, optional):  Use the UTC timezone when building a datetime object from a timestamp. Defaults to True.
+
+    Raises:
+        TypeError: Raises TypeError if the type of ``d`` is not one of the following: float, str, None, datetime, int
+
+    Returns:
+        str: Formated date and time.
+
+    Examples:
+    --------
+    >>> fmt_datetime()
+    '2022-11-22 22:40:04' # current date and time
+    >>> fmt_datetime(0)
+    "1970-01-01 00:00:00"
+    >>> fmt_datetime(0, fmt="dmY", d_sep="/")
+    "01/01/1970 00:00:00"
+    """
     if d is None:
-        d = datetime.fromtimestamp(time.time())
+        d = datetime.fromtimestamp(time.time(), **{"tz": timezone.utc} if utc else {})
     elif isinstance(d, str):
-        d = parse_datetime(d)
-    elif isinstance(d, float):
-        d = datetime.fromtimestamp(d)
+        d = parse_datetime_str(d)
+    elif isinstance(d, (float, int)):
+        d = datetime.fromtimestamp(d, **{"tz": timezone.utc} if utc else {})
     elif isinstance(d, datetime):
         pass
     else:
@@ -45,25 +68,75 @@ def fmt_datetime(
     return d.strftime(f"{d_fmt} {t_fmt}")
 
 
-def fmt_time(t: float | None = None, sep: str = ":", ms: bool = False) -> str:
+def fmt_time(
+    t: float | datetime | int | None = None, sep: str = ":", ms: bool = False, utc: bool = True
+) -> str:
+    """
+    Format time
+
+    Args:
+        t (float | datetime | int | None, optional): Input time. Defaults to current time if it's not provided.
+        sep (str, optional): Character(s) that seperates hours, minutes, seconds... Defaults to ":".
+        ms (bool, optional): include miliseconds. Defaults to False.
+        utc (bool, optional): Use the UTC timezone when building a time object from a timestamp. Defaults to True.
+
+    Raises:
+        TypeError: Raises TypeError if the type of ``t`` is not one of the following: float, None, datetime, int
+
+    Returns:
+        str: Formated time.
+    """
     if t is None:
-        t = time.time()
-    tm = datetime.fromtimestamp(t)
+        tm = datetime.fromtimestamp(time.time(), **{"tz": timezone.utc} if utc else {})
+    elif isinstance(t, datetime):
+        tm = t.time()
+    elif isinstance(t, (int, float)):
+        tm = datetime.fromtimestamp(t, **{"tz": timezone.utc} if utc else {})
+    else:
+        raise TypeError(type(t))
     ts = tm.strftime(f"%H{sep}%M{sep}%S")
     if ms:
-        ts = f"{ts}.{int(tm.microsecond / 1000)}"
+        ms_str = f"{int(tm.microsecond / 1000)}".zfill(3)
+        ts = f"{ts}.{ms_str}"
     return ts
 
 
 def fmt_date(
-    d: float | None | date = None,
+    d: float | date | datetime | int | None = None,
     fmt: str = "Ymd",
     sep: str = "-",
 ) -> str:
+    """
+    Format date
+
+    Args:
+        d (float | date | datetime | int | None ): Input date. Defaults to current date if it's not provided.
+        fmt (str, optional): Date format. Defaults to "Ymd".
+        sep (str, optional): Character(s) that seperates days, months and years. Defaults to "-".
+
+    Raises:
+        TypeError: Raises TypeError if the type of ``d`` is not one of the following: float, None, date, datetime, int
+
+    Returns:
+        str: Formated date.
+
+    Examples
+    --------
+    >>> fmt_date(date(2022,11,22))
+    '2022-11-22'
+    >>> fmt_date(date(2022,11,22), sep="/")
+    '2022/11/22'
+    >>> fmt_date(date(2022,11,22), sep="/", fmt="dmY")
+    '22/11/2022'
+    >>> fmt_date()
+    '2022-11-22'
+    """
     if d is None:
         d = date.fromtimestamp(time.time())
-    elif isinstance(d, float):
+    elif isinstance(d, (float, int)):
         d = date.fromtimestamp(d)
+    elif isinstance(d, datetime):
+        d = d.date()
     elif isinstance(d, date):
         pass
     else:
@@ -72,16 +145,30 @@ def fmt_date(
 
 
 def date_range(start: date, end: date) -> Generator[date, None, None]:
-    """Returns a generator for dates between ``start`` and ``end``"""
+    """
+    Returns a generator for dates between ``start`` and ``end``
+
+    Examples
+    --------
+    >>> for i in date_range(date(2022,11,19), date(2022,11,22)): print(i)
+    2022-11-19
+    2022-11-20
+    2022-11-21
+    """
     for n in range(int((end - start).days)):
         yield start + timedelta(n)
 
 
 __all__ = [
     "Timer",
-    "parse_datetime",
+    "parse_datetime_str",
     "fmt_datetime",
     "fmt_time",
     "fmt_date",
     "date_range",
+    "local_tz",
+    "timezone",
+    "date",
+    "datetime",
+    "time",
 ]
