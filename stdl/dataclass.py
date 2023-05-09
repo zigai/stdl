e7c56737e0731395e5ef0e13e4c721604c8c4538
zigai <ziga.ivansek@gmail.com>
2023-05-09 19:28:42 +0200

    extra dataclasss methods

--- stdl/dataclass.py
+++ stdl/dataclass.py
@@ -1,5 +1,8 @@
 from dataclasses import asdict, astuple, dataclass
 from pprint import pprint
+from typing import Any, Mapping
+
+from stdl.fs import Pathlike, json_dump
 
 
 class Data:
@@ -8,11 +11,11 @@ class Data:
 
     @property
     def dict(self) -> dict:
-        return asdict(self)
+        return asdict(self)  # type:ignore
 
     @property
     def tuple(self) -> tuple:
-        return astuple(self)
+        return astuple(self)  # type:ignore
 
     def print(self):
         pprint(self.dict)
@@ -23,6 +26,13 @@ class Data:
     def __iter__(self):
         for i in self.tuple:
             yield i
+
+    @classmethod
+    def from_dict(cls, d: Mapping[str, Any]):
+        return cls(**d)
+
+    def json_dump(self, filepath: Pathlike, endcoding="utf-8", indent=4):
+        json_dump(self.dict, filepath, endcoding, indent=indent)
 
 
 __all__ = ["Data", "dataclass"]
