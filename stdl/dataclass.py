8b6d19f5c34bd50df391e37b68a4297add4b31f5
zigai <ziga.ivansek@gmail.com>
2023-05-11 23:14:44 +0200

    nicer Data class

--- stdl/dataclass.py
+++ stdl/dataclass.py
@@ -1,38 +1,44 @@
+import json
+from copy import deepcopy
 from dataclasses import asdict, astuple, dataclass
 from pprint import pprint
 from typing import Any, Mapping
 
-from stdl.fs import Pathlike, json_dump
+from stdl.fs import Pathlike, json_load
 
 
 class Data:
     def __init__(self) -> None:
         pass
 
-    @property
+    def __getitem__(self, key):
+        return self.dict()[key]
+
+    def __iter__(self):
+        for i in self.tuple():
+            yield i
+
     def dict(self) -> dict:
         return asdict(self)  # type:ignore
 
-    @property
     def tuple(self) -> tuple:
         return astuple(self)  # type:ignore
 
-    def print(self):
-        pprint(self.dict)
+    def json(self):
+        return json.dumps(self.dict())
 
-    def __getitem__(self, key):
-        return self.dict[key]
-
-    def __iter__(self):
-        for i in self.tuple:
-            yield i
+    def copy(self):
+        return deepcopy(self)
 
     @classmethod
-    def from_dict(cls, d: Mapping[str, Any]):
-        return cls(**d)
+    def parse_obj(cls, obj: Mapping[str, Any]):
+        return cls(**obj)
 
-    def json_dump(self, filepath: Pathlike, endcoding="utf-8", indent=4):
-        json_dump(self.dict, filepath, endcoding, indent=indent)
+    def parse_file(self, filepath: Pathlike):
+        return self.parse_obj(json_load(filepath))  # type:ignore
+
+    def print(self):
+        pprint(self.dict)
 
 
 __all__ = ["Data", "dataclass"]
