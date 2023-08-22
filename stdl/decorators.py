8d2345e7ac997b0a325760b615419e371dd15d7c
zigai <ziga.ivansek@gmail.com>
2023-08-22 20:20:50 +0200

    bump version

---
+++ stdl/decorators.py
@@ -0,0 +1,86 @@
+import inspect
+import json
+import sys
+import time
+from functools import wraps
+from typing import Callable
+
+
+def timer(
+    r: int | None = 2,
+    sink: Callable = sys.stdout.write,
+    show_args: bool = False,
+    serialize: bool = False,
+    sep=" | ",
+):
+    """
+    A decorator that displays the time taken for a function to execute.
+
+    Args:
+        r (int, optional): Number of decimal places to round the time to. Defaults to 2.
+        sink (Callable, optional): A function that takes a string and does something with it. Defaults to print.
+        show_args (bool, optional): Whether to show the arguments passed to the function. Defaults to False.
+        serialize (bool, optional): Whether to serialize the output as JSON. Defaults to False.
+        sep (str, optional): Separator between the function/method name and the arguments. Defaults to " | ".
+    """
+
+    def decorator(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            time_start = time.perf_counter()
+            result = func(*args, **kwargs)
+
+            time_end = time.perf_counter()
+            time_elapsed = time_end - time_start
+            if r is not None:
+                time_elapsed = round(time_elapsed, r)
+
+            is_method = "self" in list(inspect.signature(func).parameters.keys())
+            args = args[1:] if is_method else args
+            name = func.__qualname__ if is_method else func.__name__
+            message = f"'{name}' took {time_elapsed}s"
+
+            if show_args:
+                kwargs_info = ", ".join(f"{k}={v}" for k, v in kwargs.items())
+                all_args = ", ".join(map(str, args)) + (", " + kwargs_info if kwargs_info else "")
+                message += f"{sep}Args: {all_args}"
+
+            if serialize:
+                message_dict = {
+                    "name": name,
+                    "time": time_elapsed,
+                    "args": args,
+                    "kwargs": kwargs,
+                }
+                if show_args:
+                    message_dict["args"] = args
+                    message_dict["kwargs"] = kwargs
+                message = json.dumps(message_dict)
+
+            sink(message)
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+def retry(attempts: int, delay: float = 0):
+    if attempts < 1:
+        raise ValueError("attempts must be greater than 0")
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for attempt in range(attempts):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    if attempt < attempts - 1:
+                        time.sleep(delay)
+                    else:
+                        raise e
+
+        return wrapper
+
+    return decorator
---
+++ tests/test_decorators.py
@@ -0,0 +1,104 @@
+import json
+import time
+
+import pytest
+
+from stdl.decorators import retry, timer
+
+
+class TestTimer:
+    def test_basic(self):
+        @timer()
+        def func():
+            return 42
+
+        assert func() == 42
+
+    def test_with_arguments(self):
+        @timer(show_args=True)
+        def func(x, y):
+            return x + y
+
+        assert func(10, 20) == 30
+
+    def test_method(self):
+        class MyClass:
+            @timer(serialize=True)
+            def method(self):
+                return "instance method"
+
+        assert MyClass().method() == "instance method"
+
+    def test_sink(self):
+        sink = []
+
+        @timer(sink=sink.append)
+        def func():
+            return 42
+
+        assert func() == 42
+        assert sink[0].startswith("'func' took")
+
+    def test_sink_serialized(self):
+        messages = []
+
+        @timer(sink=messages.append, serialize=True)
+        def func():
+            return 42
+
+        assert func() == 42
+        message_dict = json.loads(messages[0])
+        assert message_dict["name"] == "func"
+        assert message_dict["time"] >= 0
+
+    def test_show_args(self):
+        sink = []
+
+        @timer(show_args=True, sink=sink.append, serialize=True)
+        def func(x, y):
+            return x * y
+
+        assert func(5, 4) == 20
+        message_dict = json.loads(sink[0])
+        assert message_dict["name"] == "func"
+        assert message_dict["time"] >= 0
+        assert message_dict["args"] == [5, 4]
+        assert message_dict["kwargs"] == {}
+
+
+class TestRetryDecorator:
+    def test_retry_success(self):
+        counter = [0]
+
+        @retry(attempts=3)
+        def successful_func():
+            counter[0] += 1
+            return "Success"
+
+        assert successful_func() == "Success"
+        assert counter[0] == 1
+
+    def test_retry_failure(self):
+        counter = [0]
+
+        def failing_func():
+            counter[0] += 1
+            raise Exception("Failure")
+
+        with pytest.raises(Exception) as excinfo:
+            failing_func()
+        assert str(excinfo.value) == "Failure"
+        assert counter[0] == 3
+
+    def test_retry_with_success_on_second_attempt(self):
+        counter = [0]
+
+        @retry(attempts=3)
+        def func_with_success_on_second():
+            counter[0] += 1
+            if counter[0] < 2:
+                raise Exception("Failure")
+            return "Success"
+
+        assert func_with_success_on_second() == "Success"
+        assert counter[0] == 2
