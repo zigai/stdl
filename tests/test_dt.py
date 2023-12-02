368789b693cb6f8d7f0b058158ffb60b9accae40
zigai <ziga.ivansek@gmail.com>
2023-12-02 14:37:53 +0100

    update tests

--- tests/test_dt.py
+++ tests/test_dt.py
@@ -1,21 +1,20 @@
 from stdl.dt import *
 
 
-def test_fmt_time():
-    assert fmt_time(0) == "00:00:00"
-    assert fmt_time(0, ms=True) == "00:00:00.000"
-    assert fmt_time(1, ms=True) == "00:00:01.000"
+def test_time_fmt():
+    assert time_fmt(0) == "00:00:00"
+    assert time_fmt(0, ms=True) == "00:00:00.000"
+    assert time_fmt(1, ms=True) == "00:00:01.000"
 
 
-def test_fmt_datetime():
-    assert fmt_datetime(0.0, utc=True) == "1970-01-01 00:00:00"
-    assert fmt_datetime(0) == "1970-01-01 00:00:00"
-    assert fmt_datetime(0, fmt="dmY") == "01-01-1970 00:00:00"
-    assert fmt_datetime(0, fmt="dmY", dsep="/") == "01/01/1970 00:00:00"
-    assert fmt_datetime(0, fmt="dmY", dsep="/", tsep=".") == "01/01/1970 00.00.00"
-    # assert fmt_datetime(0.0, utc=False) == "1970-01-01 01:00:00"
+def test_datetime_fmt():
+    assert datetime_fmt(0.0, utc=True) == "1970-01-01 00:00:00"
+    assert datetime_fmt(0) == "1970-01-01 00:00:00"
+    assert datetime_fmt(0, fmt="dmY") == "01-01-1970 00:00:00"
+    assert datetime_fmt(0, fmt="dmY", dsep="/") == "01/01/1970 00:00:00"
+    assert datetime_fmt(0, fmt="dmY", dsep="/", tsep=".") == "01/01/1970 00.00.00"
 
 
-def test_fmt_date():
-    assert fmt_date(date(1970, 1, 1)) == "1970-01-01"
-    assert fmt_date(0) == "1970-01-01"
+def test_date_fmt():
+    assert date_fmt(date(1970, 1, 1)) == "1970-01-01"
+    assert date_fmt(0) == "1970-01-01"
