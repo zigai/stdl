bafd9661300e20e0f4f39fa786796fd7b74dab41
zigai <ziga.ivansek@gmail.com>
2026-06-15 10:47:46 +0200

    feat: lazy-load package exports in __init__

--- stdl/__init__.py
+++ stdl/__init__.py
@@ -1,2 +1,39 @@
-from stdl import color, decorators, dt, fs, import_lazy, log, lst, net, st
-from stdl.fs import AsyncFileHandle, Directory, File
+from __future__ import annotations
+
+from types import ModuleType
+from typing import TYPE_CHECKING, TypeAlias
+
+if TYPE_CHECKING:
+    from stdl.fs import AsyncFileHandle, Directory, File
+
+LazyExport: TypeAlias = "ModuleType | type[AsyncFileHandle[str] | Directory | File]"
+
+
+def __getattr__(name: str) -> LazyExport:
+    """Lazily resolve public package exports."""
+    if name in {"AsyncFileHandle", "Directory", "File"}:
+        from stdl import fs
+
+        return getattr(fs, name)
+    if name in {"color", "decorators", "dt", "fs", "import_lazy", "log", "lst", "net", "st"}:
+        import importlib
+
+        return importlib.import_module(f"stdl.{name}")
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+
+__all__ = [
+    "AsyncFileHandle",
+    "Directory",
+    "File",
+    "color",
+    "decorators",
+    "dt",
+    "fs",
+    "import_lazy",
+    "log",
+    "lst",
+    "net",
+    "st",
+]
---
+++ stdl/__init__.pyi
@@ -0,0 +1,27 @@
+from stdl import color as color
+from stdl import decorators as decorators
+from stdl import dt as dt
+from stdl import fs as fs
+from stdl import import_lazy as import_lazy
+from stdl import log as log
+from stdl import lst as lst
+from stdl import net as net
+from stdl import st as st
+from stdl.fs import AsyncFileHandle as AsyncFileHandle
+from stdl.fs import Directory as Directory
+from stdl.fs import File as File
+
+__all__ = [
+    "AsyncFileHandle",
+    "Directory",
+    "File",
+    "color",
+    "decorators",
+    "dt",
+    "fs",
+    "import_lazy",
+    "log",
+    "lst",
+    "net",
+    "st",
+]
